712e97ec8c062c5a4fb9e7aae4a813cae012a65e
LGoyvaerts <lorris.goyvaerts@ti8m.ch>
2019-11-11 05:25:03 -0800

    Added example script file

---
+++ src/main/resources/public/test.sql
@@ -0,0 +1,7 @@
+INSERT INTO category (name) VALUES('Administration');
+INSERT INTO job (name) VALUES('KV');
+INSERT INTO entry (checkIn, checkOut, category_id) VALUES("2019-10-29T11:55:10.821Z", "2019-10-29T11:55:12.821Z", 1);
+
+SELECT * FROM category;
+SELECT * FROM job;
+SELECT * FROM entry;
